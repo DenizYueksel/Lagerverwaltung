31be00eb4c291f70d1fc9002fa5f69bbaddd0862
Deniz Yüksel <denyueksel@tsn.at>
2019-12-11 16:00:15 +0100

    Lagerverwaltung Projekt

---
+++ src/test/java/Deniz/Lagerverwaltung/LagerverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package Deniz.Lagerverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagerverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
